43435792fa651e058d2089e4817db41e0f2ad2e5
baihaqi <Boihaki43@gmail.com>
2022-04-14 15:57:57 +0700

    Commit Pertama

---
+++ Projek_Final_Stock_Barang/src/main/java/com/mycompany/projek_final_stock_barang/Projek_Final_Stock_Barang.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.projek_final_stock_barang;
+
+/**
+ *
+ * @author Baihaqi
+ */
+public class Projek_Final_Stock_Barang {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
